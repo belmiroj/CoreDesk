c1d414f315fbf3252482741ca52b8c8025ff12d6
Belmiro <belmiro1991@gmail.com>
2026-09-19 13:16:30 -0300

    feat: adjusting root directory

--- Enums/TicketPriority.cs
+++
@@ -1,9 +0,0 @@
-namespace CoreDesk.API.Enums
-{
-    public enum TicketPriority
-    {
-        Low = 1,
-        Medium = 2,
-        High = 3
-    }
-}
--- Enums/TicketStatus.cs
+++
@@ -1,9 +0,0 @@
-namespace CoreDesk.API.Enums
-{
-    public enum TicketStatus
-    {
-        Open = 1,
-        InProgress = 2,
-        Closed = 3
-    }
-}
